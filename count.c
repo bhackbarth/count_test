68b7202e6deaaeca8d7827c5cbdf75536c9c0211
Brayton Hackbarth <brayton.hackbarth@gmail.com>
2022-11-15 13:10:55 -0600

    Initial commit

---
+++ count.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    unsigned int n = 0;
+    while (n < 1000000000)
+    {
+        n++;
+    }
+    printf("%d", n);
+}
---
+++ count.go
@@ -0,0 +1,10 @@
+package main
+
+// Count to 1 Billion
+func main() {
+	var n uint = 0
+	for n < 1000000000 {
+		n += 1
+	}
+	print(n)
+}
